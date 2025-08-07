5da785173a278f79af29a49615f0806947940020
hsc-mizuguchi <j.mizuguchi@hazukisystem.com>
2025-08-07 17:10:59 +0900

    tiptap editor

---
+++ frontend/src/components/Editor/EditorToolbar/TextFormatControls.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="controller">
+    <button
+      @click="editor.chain().focus().toggleBold().run()"
+      :class="{ 'is-active': editor.isActive('bold') }"
+    >
+      太字
+    </button>
+    <button
+      @click="editor.chain().focus().toggleItalic().run()"
+      :class="{ 'is-active': editor.isActive('italic') }"
+    >
+      斜体
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { inject } from "vue";
+
+const editor = inject("editor");
+</script>
+
+<style lang="css" scoped>
+.controller {
+  display: flex;
+  justify-content: flex-start;
+  margin-bottom: 10px;
+  flex-direction: column;
+}
+.toolbar button {
+  padding: 5px 10px;
+  margin-bottom: 5px;
+  border: 1px solid #ccc;
+  background: #fff;
+  cursor: pointer;
+  writing-mode: horizontal-tb;
+}
+
+.toolbar button.is-active {
+  background: #000;
+  color: #fff;
+}
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,6 +9,8 @@ import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import { aliases, mdi } from "vuetify/iconsets/mdi";
 
+import "./assets/css/editor.css"; // エディタのスタイルをインポート
+
 const vuetify = createVuetify({
   components,
   directives,
